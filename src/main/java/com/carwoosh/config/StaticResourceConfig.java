b87fcc20ad21eeaf157a70b099b27c1e05588624
Kamal <61450609+kamal5257@users.noreply.github.com>
2025-09-16 13:44:10 +0530

    upload config

---
+++ src/main/java/com/carwoosh/config/StaticResourceConfig.java
@@ -0,0 +1,18 @@
+package com.carwoosh.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Serve /uploads/** URLs from the "uploads" folder in project root
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/")
+                .setCachePeriod(3600); // optional cache in seconds
+    }
+}
+
